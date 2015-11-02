8ff375ee44ba3fee8ceb9314b2147c1d1bc5c7df
victor Rojas <victor.rojas@ec.pe>
2015-11-02 17:19:34 -0500

    cambio

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -15,6 +15,9 @@ class HomeController extends Controller
 
         $this->data['uri'] = 'hola';
 
+        $data = file_get_html('http://peru.com');
+        echo $data;
+
         return view('home.home',$this->data);
 
     }
