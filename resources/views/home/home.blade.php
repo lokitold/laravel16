35f031bcb9bab12785a0f838a1c25300974a5980
victor Rojas <victor.rojas@ec.pe>
2015-11-05 12:09:32 -0500

    cambio

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -17,7 +17,16 @@
             mapTypeId:google.maps.MapTypeId.ROADMAP
         };
         var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+        var latLng = new google.maps.LatLng(51.508742,-0.120850);
+        var marker = new google.maps.Marker({
+            position: latLng,
+            map: map
+        });
     }
+
+
+
     google.maps.event.addDomListener(window, 'load', initialize);
 </script>
 </head>
