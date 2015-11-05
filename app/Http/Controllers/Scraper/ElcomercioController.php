ce3b254cc85a6c962b82200054fe5942b8140522
victor Rojas <victor.rojas@ec.pe>
2015-11-05 15:47:08 -0500

    cmbio

--- app/Http/Controllers/Scraper/ElcomercioController.php
+++ app/Http/Controllers/Scraper/ElcomercioController.php
@@ -13,11 +13,18 @@ use App\Http\Controllers\Controller;
 
 class ElcomercioController extends Controller
 {
+    const LIST_ITEM = 'section[id=ec-ultimas] article header h2 a';
+
     public function getIndex(){
 
         $data = file_get_html('http://elcomercio.pe');
+        $sectionUltimasNoticias = $data->find(self::LIST_ITEM);
 
-        echo $data;
+
+        foreach($sectionUltimasNoticias as $link ):
+            echo '<br>'.$url = $link->getAttribute('href');
+
+        endforeach;
 
     }
 }
