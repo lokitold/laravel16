c9ddd94e29b39b2397c3559f6318f8e041fca529
victor Rojas <victor.rojas@ec.pe>
2015-11-10 10:48:16 -0500

    cambio

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: victor
+ * Date: 02/11/15
+ * Time: 02:04 PM
+ */
+
+namespace App\Http\Controllers;
+
+
+class DashboardController extends Controller
+{
+    public function getIndex(){
+
+        $this->data['uri'] = 'hola';
+
+        return view('home.home',$this->data);
+
+    }
+}
