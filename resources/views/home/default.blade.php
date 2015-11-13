014daadcd1c0e0f2e4d11b36dbc95653cf9d77f4
victor Rojas <victor.rojas@ec.pe>
2015-11-13 14:46:25 -0500

    cambio

--- resources/views/home/default.blade.php
+++ resources/views/home/default.blade.php
@@ -9,52 +9,9 @@
 <script src="/sbadmin2/bower_components/jquery/dist/jquery.min.js"></script>
 <script src="https://maps.googleapis.com/maps/api/js"></script>
 <script>
-
     var misPuntos = <?php echo $marcadoresJson ?>;
-
-    function initialize() {
-        var mapCanvas = document.getElementById('map');
-
-        var mapOptions = {
-            center: new google.maps.LatLng(-12.0461738, -77.0299262),
-            zoom: 2,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        }
-        var map = new google.maps.Map(mapCanvas, mapOptions)
-
-        setGoogleMarkers(map, misPuntos);
-
-    }
-
-    var markers = Array();
-    var infowindowActivo = false;
-    function setGoogleMarkers(map, locations) {
-
-        var marker;
-        var infowindow = new google.maps.InfoWindow();
-
-        for (i in locations){
-
-            var marcador = locations[i];
-            var myLatLng = new google.maps.LatLng(marcador.latitud, marcador.longitud);
-
-            marker = new google.maps.Marker({
-                position:myLatLng,
-                map: map
-            });
-
-            google.maps.event.addListener(marker, 'click', (function(marker, i) {
-                return function() {
-                    infowindow.setContent(locations[i].content);
-                    infowindow.open(map, marker);
-                }
-            })(marker, i));
-
-        }
-    }
-
-    google.maps.event.addDomListener(window, 'load', initialize);
 </script>
+<script src="/laravel16/home.js"></script>
 <div class="container">
     <div class="row">
         @if($errors->has())
