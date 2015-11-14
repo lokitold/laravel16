3fb0a8e5bd397c97e5e6b7c552cab16db97872fd
victor <victor.rojas@ec.pe>
2015-11-14 10:50:15 -0500

    cambio

--- resources/views/dashboard/noticia/createUpdate.blade.php
+++ resources/views/dashboard/noticia/createUpdate.blade.php
@@ -10,7 +10,7 @@
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
                 <div class="panel-heading">
-                    <p class="text-right"><a href="{!! $noticia->url !!}}">Acceder a Noticia</a></p>
+                    <p class="text-right"><a target='_blank' href="{!! $noticia->url !!}}">Acceder a Noticia</a></p>
                 </div>
 
                 @if($errors->has())
