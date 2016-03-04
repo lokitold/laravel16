e3497695dc0054a6ee6aaba32e3c29173001dd1b
victor Rojas <victor.rojas@ec.pe>
2016-03-04 18:41:13 -0500

    cambio

--- resources/views/dashboard/noticia/createUpdate.blade.php
+++ resources/views/dashboard/noticia/createUpdate.blade.php
@@ -28,9 +28,9 @@
                 <div class="panel-body">
                     {!! Form::model($noticia,['route' => ['dashboard.noticia.update', $noticia->id],'method' => 'PUT']) !!}
 
-                    <div class="form-group">
+                            <!--div class="form-group">
                         {!! Form::submit('Send', ["class" => "btn btn-success "]) !!}
-                    </div>
+                            </div-->
 
                     <div class="form-group">
                         {!! Form::text('titulo', null, ["class" => "form-control" , 'disabled' => 'disabled']) !!}
@@ -61,6 +61,7 @@
                     </div>
 
                     <!-- test varios locaciones por noticias -->
+                    {!! Form::label('locacioness', 'Localizaciones de la nota') !!}
                     <div class="panel panel-default" style="border-color : #A9A6A6">
                         <div class="panel-body">
                             <div class="row">
@@ -97,6 +98,7 @@
 
                     <div class="panel-body ">
                         <input id="pac-input" class="form-control" type="text" placeholder="Buscar...">
+
                         <div id="map" style="height: 620px"></div>
                     </div>
 
@@ -104,15 +106,14 @@
                         <p class="text-left"><a href="{!! $noticia->url !!}}" target="_blank">Acceder a Noticia</a></p>
                     </div>
 
-                     <div class="form-group">
+                    <div class="form-group">
                         {!! Form::label('estado', 'Estado') !!}
                         {!! Form::select('status', ['' => '', 0 => 'Sin Editar', 1 => 'Editado', 2 => 'Rechazado'], null, ['class' => 'form-control']) !!}
                     </div>
 
                     <div class="form-group">
-                        {!! Form::submit('Send', ["class" => "btn btn-success "]) !!}
+                        {!! Form::submit('Agregate location', ["class" => "btn btn-success "]) !!}
                     </div>
-
 
 
                     {!! Form::close() !!}
@@ -126,7 +127,8 @@
             var latLng = {
                         lat: <?php echo $noticia->latitud ? $noticia->latitud : '-12.0461738' ?>,
                         lng: <?php echo $noticia->longitud ? $noticia->longitud : '-77.0299262' ?>
-              },
+
+                    },
                     map = new google.maps.Map(document.getElementById('map'), {
                         center: latLng,
                         zoom: 12,
@@ -138,7 +140,7 @@
                         map: map,
                         title: 'Arrastrar para ubicar posición'
                     }),
-                    setData = function (){
+                    setData = function () {
                         document.getElementById('input-longitud').value = marker.position.lng();
                         document.getElementById('input-latitud').value = marker.position.lat();
                         $('select[name=\'status\'] > option[value="1"]').attr('selected', 'selected')
@@ -146,16 +148,16 @@
                     input = document.getElementById('pac-input'),
                     searchBox = new google.maps.places.SearchBox(input);
 
-            map.addListener('click', function(e) {
+            map.addListener('click', function (e) {
                 marker.setPosition(e.latLng);
                 setData();
             });
             map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-            marker.addListener('dragend', function(){
+            marker.addListener('dragend', function () {
                 setData();
             });
 
-            searchBox.addListener('places_changed', function() {
+            searchBox.addListener('places_changed', function () {
                 var places = searchBox.getPlaces();
                 if (places.length == 0) {
                     return;
@@ -169,6 +171,7 @@
         }
     </script>
     <script src="/sbadmin2/bower_components/jquery/dist/jquery.min.js"></script>
-    <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA2g6-YR2236S6r66mOjlNYE-eoiVvETvw&signed_in=true&libraries=places&callback=initAutocomplete"></script>
+    <script async defer
+            src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA2g6-YR2236S6r66mOjlNYE-eoiVvETvw&signed_in=true&libraries=places&callback=initAutocomplete"></script>
 @endsection
 
