3182ee78b8fc2ddd5ffdb5f40dc2b1b84bd2a0c5
victor-pc <vico.16c@gmail.com>
2016-03-06 14:42:29 -0500

    cmvio

--- app/Http/Controllers/Test/TestController.php
+++ app/Http/Controllers/Test/TestController.php
@@ -13,6 +13,11 @@ use App\Http\Controllers\Controller;
 class TestController extends Controller
 {
 
+    # geocodign test
+    # Api => http://maps.googleapis.com/maps/api/geocode/output?parameters
+    # Clave del api  = AIzaSyDI1G5t3_HL8rXgGiK8HrpLu5yRWyTqPSo
+    # Administrador de api de google => https://console.developers.google.com
+
     public function test(){
         echo "aca";
     }
