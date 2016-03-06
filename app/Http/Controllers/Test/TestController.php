47dd12908687f56ac5773ff5db7e8993ccb248fa
victor-pc <vico.16c@gmail.com>
2016-03-06 00:13:56 -0500

    cambio

---
+++ app/Http/Controllers/Test/TestController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: victor
+ * Date: 02/11/15
+ * Time: 02:04 PM
+ */
+
+namespace App\Http\Controllers\Test;
+
+use App\Http\Controllers\Controller;
+
+class TestController extends Controller
+{
+
+    public function test(){
+        echo "aca";
+    }
+}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -18,6 +18,7 @@ Route::get('/test', ['as' =>'test', 'uses' => 'HomeController@test']);
 //Route test
 Route::get('/testapi', ['as' =>'testapi', 'uses' => 'TestController@test']);
 Route::get('/test-locations', ['as' =>'testlocations', 'uses' => 'TestController@testLocation']);
+Route::get('/test/googlemaps', ['as' =>'testgooglemaps', 'uses' => 'Test\TestController@test']);
 
 // Dashboard
 Route::group(['middleware' => 'auth'], function () {
